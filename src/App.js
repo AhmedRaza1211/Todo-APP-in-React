19c18c9bcd3ae4e29efd157b65e398def0b70cea
AhmedRaza1211 <ahmedraza8325@gmail.com>
2022-02-08 17:53:52 +0500

    edit and delete functionality

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import Todo from './component/todo';
 function App() {
   return (
     <div>
-      <h2> Ahmed Raza Attari </h2>
+      {/* <h2> Ahmed Raza Attari </h2>
       <p> <Paragraph/> </p>
       <p> <One /> </p>
       <p> <Three /> </p>
       <p> <JustOne /> </p>
 
       <h1> Props </h1>
-      <Pass2 />
+      <Pass2 /> */}
 
       {/* TODO APP */}
       <Todo />
--- src/component/todo.js
+++ src/component/todo.js
@@ -2,14 +2,40 @@ import { useState } from "react";
 
 const Todo = () => {
     const [todos, setTodos] = useState([]);
-    let inputValue = ''
-    const onInputChange = (event) =>  inputValue = event.target.value;
-    const addTodo = () => setTodos([...todos, inputValue]);
-    const showTodo = todos.map((v,i) => <li key={i}> {v} </li>)
+    const [inputValue, setInputValue] = useState('');
+    let onInputChange = (event) => setInputValue(event.target.value);
+    const addTodo = () => {
+        setTodos([...todos, inputValue]);
+        setInputValue('');
+    };
+
+    // Delete Single Item of Todo
+    const deleteItem = (index) => {
+        console.log(index);
+        let arr = [...todos];
+        console.log(arr);
+        arr.splice(index, 1);
+        setTodos(arr);
+    }
+
+    // Edit Todo
+    const editItem = (index) => {
+        console.log(index);
+        let editQues = prompt('Edit Todo');
+        let arr = [...todos];
+        arr.splice(index, 1, editQues);
+        setTodos(arr);
+    }
+    
+
+    const showTodo = todos.map((v, i) => <li key={i}> {v}
+        <button onClick={() => deleteItem(i)}> Delete </button>
+        <button onClick={() => editItem(i)}> Edit </button>
+    </li>)
 
     return <div>
         <h1> Todo App </h1>
-        <input placeholder='Enter Todo' onChange={onInputChange} />
+        <input value={inputValue} placeholder='Enter Todo' onChange={onInputChange} />
         <button onClick={addTodo}> Add Todo </button>
         <ol>
             {showTodo}
